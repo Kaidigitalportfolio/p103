fcc5e2f1086110a2f597ea5f0de2b5e2935b517c
Kai Goodsell <kgoodsell@wisc.edu>
2026-04-29 23:04:03 -0500

    made small changes

--- src/pages/SkiHillsPage.jsx
+++ src/pages/SkiHillsPage.jsx
@@ -1,6 +1,5 @@
 import { useMemo, useState } from 'react'
 import { Container, Row, Col, Form } from 'react-bootstrap'
-import HillStats from '../components/HillStats'
 import PageIntro from '../components/PageIntro'
 import SkiHillCard from '../components/SkiHillCard'
 import { skiHills } from '../data/skiHills'
@@ -87,7 +86,6 @@ function SkiHillsPage() {
         title="Ski Hills"
         description="Filter Wisconsin ski areas by distance, vertical drop, terrain, and name."
       />
-      <HillStats hills={filteredHills} />
       <Form className="filter-card mb-4">
         <Row className="g-3 align-items-end">
           <Col xs={12} md={4} lg={3}>
